8caf1c71de923e8ee0496c983442cd20ce080d83
jungyu09 <jungyu09@gmail.com>
2020-12-02 00:23:34 +0900

    HelloController, hello.jsp 구현

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -7,16 +7,19 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+//EnableWebMvc애노테이션은 스프링 MVC 설정을 활성화한다. 스프링 MVC를 사용하는데 필요한 다양한 설정을 생성한다.
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer{
 
 	@Override
+	//DispatcherServlet의 매핑 경로를 '/'로 주었을 때, JSP/HTML/CSS 등을 올바르게 처리하기 위한 서정을 추가한다.
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		// TODO Auto-generated method stub
 		configurer.enable();
 	}
 
 	@Override
+	//JSP를 이용해서 컨트롤러의 실행 결과를 보여주기 위한 설정을 추가한다
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		// TODO Auto-generated method stub
 		registry.jsp("/WEB-INF/view/",".jsp");
