a557b4b3fffb0f09e4e215dc70167a1de3bccfe2
Lee-Jungyu <jungyu09@gmail.com>
2020-12-15 21:26:57 +0900

    Implement login

--- src/main/java/config/ControllerConfig.java
+++ src/main/java/config/ControllerConfig.java
@@ -1,15 +1,30 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import controller.HelloController;
+import controller.RegisterController;
+import service.MemberRegisterService;
 
 @Configuration
+@Import(AppCtx.class)
 public class ControllerConfig {
+	
+	@Autowired
+	private MemberRegisterService memberRegSvc;
 	
 	@Bean
 	public HelloController helloController() {
 		return new HelloController();
+	}
+	
+	@Bean
+	public RegisterController registerController() {
+		RegisterController controller = new RegisterController();
+		controller.setMemberRegisterService(memberRegSvc);
+		return controller;
 	}
 }
