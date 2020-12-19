32e278308fb5c8ee4c73e8757f4cc1ef18e5def2
Lee-Jungyu <jungyu09@gmail.com>
2020-12-19 17:05:36 +0900

    회원가입 에러메시지 출력 구현

--- src/main/java/controller/register/RegisterController.java
+++ src/main/java/controller/register/RegisterController.java
@@ -1,6 +1,7 @@
 package controller.register;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,11 +43,15 @@ public class RegisterController {
 	
 	@PostMapping("/register/step3")
 	// RegisterRequest 파라미터를 step3.jsp에서 ${registerRequest.name}으로 사용 
-	public String handleStep3(RegisterRequest regReq) {
+	public String handleStep3(RegisterRequest regReq, Errors errors) {
+		new RegisterRequestValidator().validate(regReq, errors);
+		if(errors.hasErrors())
+			return "register/step2";
 		try {
 			memberRegisterService.regist(regReq);
 			return "register/step3";
 		} catch(DuplicateMemberException e) {
+			errors.rejectValue("email", "duplicate");
 			return "register/step2";
 		}
 	}
